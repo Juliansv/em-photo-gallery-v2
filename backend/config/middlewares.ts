b5891bd9a97979bcb5f91cb1b0ca2b35e77d418b
Juliansv <juliansv22@gmail.com>
2024-11-17 17:54:09 +0100

    Initial commit

---
+++ backend/config/middlewares.ts
@@ -0,0 +1,37 @@
+export default [
+	"strapi::logger",
+	"strapi::errors",
+	{
+		name: "strapi::security",
+		config: {
+			contentSecurityPolicy: {
+				useDefaults: true,
+				directives: {
+					"connect-src": ["'self'", "https:"],
+					"img-src": [
+						"'self'",
+						"data:",
+						"blob:",
+						"dl.airtable.com",
+						"strapi-em-gallery-aws-s3-images-bucket.s3.sa-east-1.amazonaws.com",
+					],
+					"media-src": [
+						"'self'",
+						"data:",
+						"blob:",
+						"dl.airtable.com",
+						"strapi-em-gallery-aws-s3-images-bucket.s3.sa-east-1.amazonaws.com",
+					],
+					upgradeInsecureRequests: null,
+				},
+			},
+		},
+	},
+	"strapi::cors",
+	"strapi::poweredBy",
+	"strapi::query",
+	"strapi::body",
+	"strapi::session",
+	"strapi::favicon",
+	"strapi::public",
+];
