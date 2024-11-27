79247c38133fe943260f0d35f12aedb5fb3f809a
Juliansv <juliansv22@gmail.com>
2024-11-28 00:19:49 +0100

    add getCollections function

--- frontend/lib/strapi-helpers.ts
+++ frontend/lib/strapi-helpers.ts
@@ -5,3 +5,11 @@ export function getHomeInfo() {
 		return res.data;
 	});
 }
+
+export function getCollections() {
+	const params =
+		"collections?populate[coverImage][fields][0]=url&fields[0]=title";
+	return query(params).then((res) => {
+		return res.data;
+	});
+}
