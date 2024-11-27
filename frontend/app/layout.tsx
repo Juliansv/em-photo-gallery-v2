c96eaaa4f89174e313aa070c5dc547e47709e8b5
Juliansv <juliansv22@gmail.com>
2024-11-28 00:19:02 +0100

    add scroll snap

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth snap-y snap-mandatory">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
