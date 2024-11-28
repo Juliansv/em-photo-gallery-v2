d546c78f69c94076d86dcc3faa1decc981e81c3c
Juliansv <juliansv22@gmail.com>
2024-11-28 23:08:41 +0100

    add scroll restorer to gallery section

--- frontend/app/components/ImageGallery.tsx
+++ frontend/app/components/ImageGallery.tsx
@@ -1,21 +1,26 @@
 import { getCollections } from "@/lib/strapi-helpers";
 import CollectionSection from "./CollectionSection";
 import { Collection } from "@/lib/types";
+import { Suspense } from "react";
+import ClientSideScrollRestorer from "./ClientSideScrollRestorer";
 
-const GallerySection = async () => {
+const ImageGallery = async () => {
 	const collections: Collection[] = await getCollections();
 
 	return (
 		<>
 			{collections.map((collection: Collection, i: number) => (
 				<CollectionSection
-                    key={i}
-                    collection={collection}
-                    collectionIndex={i}
+					key={i}
+					collection={collection}
+					collectionIndex={i}
 				/>
 			))}
+			<Suspense>
+				<ClientSideScrollRestorer />
+			</Suspense>
 		</>
 	);
 };
 
-export default GallerySection;
+export default ImageGallery;
