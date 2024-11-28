459cd5a1ba32d40c7adfa9b0d79bc42e08b8ce30
Juliansv <juliansv22@gmail.com>
2024-11-28 23:07:57 +0100

    change overflow from hidden to clip

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,29 +2,31 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
+
 const PorterSans = localFont({
-    src: "./fonts/PorterSans.woff",
-    variable: "--font-porter-sans",
-    weight: "100 900",
-})
+	src: "./fonts/PorterSans.woff",
+	variable: "--font-porter-sans",
+	weight: "100 900",
+});
 
 export const metadata: Metadata = {
-  title: "ESV Film Photos",
-  description: "Gallery of film photos",
+	title: "ESV Film Photos",
+	description: "Gallery of film photos",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" className="scroll-smooth snap-y snap-mandatory">
-      <body
-        className={`${PorterSans.className} antialiased overflow-x-hidden`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" className="scroll-smooth snap-y snap-mandatory">
+			<body
+				className={`${PorterSans.className} antialiased overflow-x-clip`}
+                id="body"
+			>
+				{children}
+			</body>
+		</html>
+	);
 }
