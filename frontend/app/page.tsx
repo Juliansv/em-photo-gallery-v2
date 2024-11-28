2987a8b23de657af59c17928bafc122f0229c24f
Juliansv <juliansv22@gmail.com>
2024-11-28 23:08:17 +0100

    change name of imageGallery

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,11 @@
 import HeroSection from "./components/HeroSection";
-import GallerySection from "./components/ImageGallery";
+import ImageGallery from "./components/ImageGallery";
 
 export default async function Home() {
-  return (
-    <article>
-      <HeroSection />
-      <GallerySection />
-    </article>
-  );
+	return (
+		<article>
+			<HeroSection />
+			<ImageGallery />
+		</article>
+	);
 }
