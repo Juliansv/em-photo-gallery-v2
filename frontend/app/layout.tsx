2b91c24576536ccce1f69db89a9b35acc7ba882f
Juliansv <juliansv22@gmail.com>
2024-11-28 01:51:55 +0100

    add overflow-x-hidden

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth snap-y snap-mandatory">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden`}
       >
         {children}
       </body>
